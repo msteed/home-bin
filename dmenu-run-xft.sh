42c7d38bca015ce885498bdb6eff80032abbbd4a
Michael Steed <msteed68@gmail.com>
2017-11-16 08:21:31 -0700

    dmenu colors

--- dmenu-run-xft.sh
+++ dmenu-run-xft.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-dmenu="dmenu -fn LiberationMono-10 -nb \#a54242 -nf \#c5c8c6 -sb \#b58900 -sf \#073642"
+dmenu="dmenu -fn Inconsolata-12 -nb \#dc322f -nf \#eee8d5 -sb \#b58900 -sf \#002b36"
 
 cachedir=${XDG_CACHE_HOME:-"$HOME/.cache"}
 if [ -d "$cachedir" ]; then
